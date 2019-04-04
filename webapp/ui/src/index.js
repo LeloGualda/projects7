b7a875b4e052757500df63d53c17bbcc755013e1
Aurelio Gualda <aurelio.10@hotmail.com>
2019-04-03 23:47:03 -0300

    starting live-ui

--- webapp/ui/src/index.js
+++ webapp/ui/src/index.js
@@ -3,29 +3,12 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {employees: []};
-	}
-
-	componentDidMount() {
-		// client({method: 'GET', path: '/api/employees'}).done(response => {
-		// 	this.setState({employees: response.entity._embedded.employees});
-        // });
-        //<EmployeeList employees={this.state.employees}/>
-
-	}
 
 	render() {
-		return (<div>
-                olá mundo
-        </div>)
+		return <div>
+            front-end
+        </div>
 	}
 }
 
-
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('react')
-)
+ReactDOM.render(<App/>,document.getElementById('react'))
